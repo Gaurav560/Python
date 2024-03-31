af3221933792c3ad6f4138b99a86049ca9d97ff9
Gaurav Sharma <gsharma1154@gmail.com>
2024-03-31 16:39:06 +0530

    basic ops

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import pandas
-print("Hi")
+print("Hi");
+
+print("hey i am great and i \"will be doing\"this\nand i am fodidn");
+
+#for multi line comment use triple quotes
+'''
+print("you are 2nd");
+print(17*13);
+'''
+
+#_ refers to the output of the previous operations
+
